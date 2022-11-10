454b0dcb44b007d5e352413f3ca2eb5918d4c273
shreyaa2798 <112149651+shreyaa2798@users.noreply.github.com>
2022-11-10 17:42:15 -0600

    Do Chinook

--- code/database.py
+++ code/database.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
 load_dotenv()
-
 DATABASE_USERNAME = os.environ["DATABASE_USERNAME"]
+
 DATABASE_PASSWORD = os.environ["DATABASE_PASSWORD"]
 DATABASE_HOST = os.environ["DATABASE_HOST"]
 DATABASE_PORT = os.environ["DATABASE_PORT"]
--- code/queries.py
+++ code/queries.py
@@ -2,21 +2,38 @@
 # How many artists are there?
 # Return a single column called "count" with a single row containing the count.
 query_1 = """
-
+    select
+         count("ArtistId")
+    from
+	    "Artist"
     """
-
+print(query_1)
 # PROBLEM 2
 # How many Artists do not have an Album associated with them?
 # Return a single column called "count" with a single row containing the count.
 query_2 = """
-
+    select
+        count(art."ArtistId")
+    from
+        "Artist" art
+    left join "Album" alb on
+        art."ArtistId" = alb."ArtistId"
+    where
+        alb."AlbumId" is null
 """
 
 # PROBLEM 3
 # How many Albums do not have an artist in the Artist table associated with them?
 # Return a single column called "count" with a single row containing the count.
 query_3 = """
-
+    select
+        count(a."ArtistId")
+    from
+        "Artist" a
+    right join "Album" a2 on
+        a."ArtistId" = a2."ArtistId"
+    where
+        a."ArtistId" is null
 """
 
 # PROBLEM 4
@@ -24,7 +41,16 @@ query_3 = """
 # Return a single column called "AC/DC Tracks",
 # in any order.
 query_4 = """
-
+select
+	tra."Name" as "AC/DC Tracks"
+from
+	"Artist" art
+join "Album" alb on
+	art."ArtistId" = alb."ArtistId"
+join "Track" tra on
+	alb."AlbumId" = tra."AlbumId"
+where
+	art."Name" = 'AC/DC'
 """
 
 # PROBLEM 5
@@ -32,7 +58,18 @@ query_4 = """
 # Return a single column called "Total Sales" with a single row containing the total.
 
 query_5 = """
-
+    select
+	    sum(inv."UnitPrice" * inv."Quantity") as "Total Sales"
+    from
+	    "Artist" art
+    join "Album" alb on
+	    art."ArtistId" = alb."ArtistId"
+    join "Track" tra on
+	    alb."AlbumId" = tra."AlbumId"
+    join "InvoiceLine" inv on
+	    tra."TrackId" = inv."TrackId"
+    where
+	    art."Name" = 'AC/DC'
 """
 
 # PROBLEM 6
@@ -43,6 +80,22 @@ query_5 = """
 # in any order.
 
 query_6 = """
+    select
+	    art."Name" as "Artist",
+	    sum(inv."UnitPrice" * inv."Quantity") as "Total Sales"
+    from
+	    "Artist" art
+    join "Album" alb on
+	    art."ArtistId" = alb."ArtistId"
+    join "Track" tra on
+	    alb."AlbumId" = tra."AlbumId"
+    join "InvoiceLine" inv on
+	    tra."TrackId" = inv."TrackId"
+    group by
+	    art."Name"
+    having
+	    sum(inv."UnitPrice" * inv."Quantity") <= 5
+
 
 """
 
@@ -53,7 +106,26 @@ query_6 = """
 # in descending order of "Total Sales".
 
 query_7 = """
-
+    with artist_sales as (
+        select 
+	        art."Name" as "Artist",
+	        sum(inv."UnitPrice" * inv."Quantity") as "Total Sales"
+        from
+	        "Artist" art
+        join "Album" alb on
+	        art."ArtistId" = alb."ArtistId"
+        join "Track" tra on
+	        alb."AlbumId" = tra."AlbumId"
+        join "InvoiceLine" inv on
+	        tra."TrackId" = inv."TrackId"
+        group by
+	        art."Name")
+    select
+	    *
+    from
+	    artist_sales
+    order by
+	    "Total Sales" desc
 """
 
 # PROBLEM 8
@@ -64,7 +136,16 @@ query_7 = """
 # Hint: this requires a self join, picking clear aliases will help.
 
 query_8 = """
-
+    select
+	    concat(report."LastName", ', ', report."FirstName") as "Name",
+	    report."Title"
+    from
+	    "Employee" report
+    join "Employee" manager on
+	    report."ReportsTo" = manager."EmployeeId"
+    where
+	    manager."LastName" = 'Mitchell'
+	    and manager."FirstName" = 'Michael'
 """
 
 # PROBLEM 9
@@ -75,7 +156,15 @@ query_8 = """
 # Hint: this requires a self join, picking clear aliases will help.
 
 query_9 = """
-
+    select
+	    concat(report."LastName", ', ', report."FirstName") as "Employee Name",
+	    report."Title" as "Employee Title",
+	    concat(manager."LastName", ', ', manager."FirstName") as "Manager Name",
+	    manager."Title" as "Manager Title"
+    from
+	    "Employee" report
+    join "Employee" manager on
+	    report."ReportsTo" = manager."EmployeeId"
 """
 
 # PROBLEM 10
@@ -86,6 +175,17 @@ query_9 = """
 # for example someone with the last name "Smith "and first name "Bob" should be "Bob, Smith"
 
 query_10 = """
+    select
+	    concat("LastName", ', ', "FirstName") as "Name",
+	    "HireDate" as "Hire Date"
+    from
+	    "Employee" e
+    where
+	    e."HireDate" = (
+	    select
+		    max("HireDate")
+	    from
+		    "Employee")
 
 """
 
@@ -95,6 +195,12 @@ query_10 = """
 # in any order.
 
 query_11 = """
+select
+	"FirstName" as "First Name",
+	"LastName" as "Last Name",
+	'2010-01-01' - "HireDate" as "Tenure"
+from
+	"Employee"
 
 """
 # PROBLEM 12
@@ -103,5 +209,24 @@ query_11 = """
 # in ascending order of tenure.
 
 query_12 = """
-
+    with tenure as (
+    select
+	    "FirstName" as "First Name",
+	    "LastName" as "Last Name",
+	    '2010-01-01' - "HireDate" as "Tenure",
+	    *
+    from
+	    "Employee" e)
+    select
+	    "First Name",
+	    "Last Name",
+	    "Tenure"
+    from
+	    tenure
+    where
+	    extract(day
+    from
+	    "Tenure")::int <7 * 356
+    order by
+	    "Tenure"
 """
